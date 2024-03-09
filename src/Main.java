5bb63ad766e9f4f458b8ad09209ee3cfad562cf7
charmi donda <bharodiyacharmi@gmail.com>
2024-03-09 12:44:53 +0000

    initial setup

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    int name = 'c';
+    String address = "30 dene gardens HA7 4TD";
+    double salary = 2021.30;
+}
+
+
+
+
+
+
+
